35d21f94ee2aeb5eae4a1f74c61400da6797a149
rayanc72 <68483052+rayanc72@users.noreply.github.com>
2023-11-23 17:51:25 -0500

    Implement login control

--- app1.py
+++ app1.py
@@ -14,6 +14,7 @@ import requests
 from PIL import Image
 from streamlit_lottie import st_lottie
 from streamlit_ketcher import st_ketcher
+import hmac
 
 
 # structure_content = None
@@ -21,6 +22,45 @@ from streamlit_ketcher import st_ketcher
 st.set_page_config(page_title="hPAME", layout="wide")
 
 st.title("hybrid :red[Perovskite Analysis and Modelling Engine]")
+
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
 
 st.divider()
 st.latex(r'''\rm Download\; a\;  structure\;  file\;  from\;  HybriD^3:''')

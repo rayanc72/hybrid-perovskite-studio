05432209177ab8576c3d45c3645c79e1d0baa651
rayanc72 <68483052+rayanc72@users.noreply.github.com>
2023-11-23 19:11:22 -0500

    Implement login control

--- registration.py
+++ registration.py
@@ -46,7 +46,7 @@ def register_user(user_id, email, hashed_password):
 
 def login_form():
     """Form with widgets to collect user information"""
-    with st.form("Credentials", key="login"):
+    with st.form(key="login"):
         st.text_input("Username", key="username")
         st.text_input("Password", type="password", key="password")
         st.form_submit_button("Log in", on_click=password_entered)
